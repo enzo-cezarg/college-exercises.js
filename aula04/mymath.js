9e4d349bbc5952239a3285e5ed945eea81ad810b
Enzo Garcia <enzoocezargarcia@gmail.com>
2024-11-04 21:34:52 -0300

    fourth-class-exercises

---
+++ aula04/mymath.js
@@ -0,0 +1,15 @@
+export function areaQuadrado(b, h) {
+    return b * h;
+}
+
+export function areaRetangulo(b, h) {
+    return b * h;
+}
+
+export function areaTriangulo(b, h) {
+    return ((b * h)/2);
+}
+
+export function areaCirculo(r) {
+    return 3.1415*(r*r);
+}
---
+++ aula04/teste-mymath.js
@@ -0,0 +1,6 @@
+import * as mymath from './mymath.js'
+
+console.log(`Área do quadrado: ${mymath.areaQuadrado(10, 10)} m2`);
+console.log(`Área do retângulo: ${mymath.areaRetangulo(15, 10)} m2`);
+console.log(`Área do triângulo: ${mymath.areaTriangulo(8, 7)} m2`);
+console.log(`Área do círculo: ${mymath.areaCirculo(8)} m2`);
